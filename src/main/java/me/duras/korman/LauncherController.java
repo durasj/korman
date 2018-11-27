ba8470a4a3387fd80f59a9c2829feae79cb5d77d
matomiskov <matomiskov@gmail.com>
2018-11-27 14:37:11 +0100

    Launcher 1.0.3 - preklikavanie okien

--- src/main/java/me/duras/korman/LauncherController.java
+++ src/main/java/me/duras/korman/LauncherController.java
@@ -1,76 +1,112 @@
 package me.duras.korman;
 
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+import javafx.fxml.*;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 
 /**
  *
  * @author martin
  */
 public class LauncherController implements Initializable {
-    
+
     @FXML
     private Button dashboardButton;
-    
-     @FXML
+
+    @FXML
     private Button bicyclesButton;
-     
-      @FXML
+
+    @FXML
     private Button notificationsButton;
-      
-       @FXML
+
+    @FXML
     private Button agentsButton;
-       
-        @FXML
+
+    @FXML
     private Button requirementsButton;
-        
-         @FXML
+
+    @FXML
     private Button settingsButton;
-    
+
+    @FXML
+    private AnchorPane defaultTable;
+
     @FXML
     private void showDashboard(ActionEvent event) {
-        System.out.println("You clicked me!");
-        dashboardButton.setText("Hello Dashboard!");
+        System.out.println("You clicked Dashboard!");
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("dashTable.fxml"));
+            defaultTable.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-     @FXML
+
+    @FXML
     private void showBicycles(ActionEvent event) {
-        System.out.println("You clicked me!");
-        bicyclesButton.setText("Hello Bike!");
+        System.out.println("You clicked Bike!");
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("bikeTable.fxml"));
+            defaultTable.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-     @FXML
+
+    @FXML
     private void showNotifications(ActionEvent event) {
-        System.out.println("You clicked me!");
-        notificationsButton.setText("Hello Notifications!");
+        System.out.println("You clicked Notifications!");
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("notifTable.fxml"));
+            defaultTable.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-     @FXML
+
+    @FXML
     private void showAgents(ActionEvent event) {
-        System.out.println("You clicked me!");
-        agentsButton.setText("Hello agents!");
+        System.out.println("You clicked agents!");
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("agentsTable.fxml"));
+            defaultTable.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-     @FXML
+
+    @FXML
     private void showRequirements(ActionEvent event) {
-        System.out.println("You clicked me!");
-        requirementsButton.setText("Hello Requirements!");
+        System.out.println("You clicked Requirements!");
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("reqTable.fxml"));
+            defaultTable.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-     @FXML
+
+    @FXML
     private void showSettings(ActionEvent event) {
-        System.out.println("You clicked me!");
-        settingsButton.setText("Hello Settings!");
+        System.out.println("You clicked Settings!");
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("settTable.fxml"));
+            defaultTable.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
