c1f83bff5781d3bf2474dda05e7518d2b24810e6
Jakub Ďuraš <jakub@duras.me>
2018-12-07 20:49:00 +0100

    Remove unused agents tree object

--- src/main/java/me/duras/korman/AgentsTreeObject.java
+++
@@ -1,7 +0,0 @@
-package me.duras.korman;
-
-import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
-
-import me.duras.korman.models.Agent;
-
-public class AgentsTreeObject extends RecursiveTreeObject<Agent> {}
