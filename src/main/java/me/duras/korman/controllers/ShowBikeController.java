f83df560de19c5d341dccb21210e6f07550e0c9e
matomiskov <matomiskov@gmail.com>
2018-12-08 20:44:01 +0100

    ShowBicycle

---
+++ src/main/java/me/duras/korman/controllers/ShowBikeController.java
@@ -0,0 +1,43 @@
+package me.duras.korman.controllers;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import me.duras.korman.models.Bicycle;
+
+public class ShowBikeController implements Initializable {
+
+    private Bicycle bicycle = null;
+
+    @FXML
+    private Label category, series, size, wmn, price, difference, year;
+
+    @FXML
+    private ImageView bikePic;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    public void setBicycle(Bicycle bicycle) {
+        this.bicycle = bicycle;
+        try {
+            Image image = new Image(new FileInputStream(bicycle.getPhotoUrl()));
+            bikePic.setImage(image);
+        } catch (FileNotFoundException e) {
+        }
+        category.setText(bicycle.getSeries());
+        series.setText(bicycle.getSeries());
+        size.setText(bicycle.getSize());
+        wmn.setText(bicycle.getSize());
+        price.setText(String.valueOf(bicycle.getPrice()));
+        difference.setText(String.valueOf(bicycle.getDiff()));
+        year.setText(String.valueOf(bicycle.getModelYear()));
+    }
+}
